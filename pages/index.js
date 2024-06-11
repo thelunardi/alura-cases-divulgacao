ff8aec085db4a1e9335ce3d108f19a6102ecce73
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2024-06-10 21:03:37 -0300

    home page and configs

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,8 @@
 import React from 'react';
-
-import Link from '../src/components/Link';
-
-function Title({ children, as }) {
-  const Tag = as;
-  return (
-    <React.Fragment>
-      <Tag>{children}</Tag>
-
-      <style jsx>
-        {`
-          ${Tag} {
-            color: red;
-          }
-        `}
-      </style>
-    </React.Fragment>
-  );
-}
+import HomeScreen from '../src/screens/HomeScreen';
 
 export default function HomePage() {
-  return (
-    <div>
-      <Title as="h1">Alura Cases - Home Page</Title>
-      <Link href="/faq" passHref>
-        Ir para a FAQ
-      </Link>
-    </div>
-  );
+    return (
+        <HomeScreen />
+    )
 }
--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,9 +1,9 @@
 import NextLink from 'next/link';
 
-export default function Link({children, href, ...props}) {
-  return (
-    <NextLink {...props} href={href} passHref>
-      {children}
-    </NextLink>
-  );
+export default function Link({ children, href, ...props}) {
+    return (
+        <NextLink href={href} passHref>
+            <a {...props}>{children}</a>
+        </NextLink>
+    );
 }
